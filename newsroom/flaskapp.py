871b28d141681c8db7797ba3a59070c1b63316fb
Petr <petr.jasek@sourcefabric.org>
2017-08-16 15:15:09 +0200

    init app.media

--- newsroom/flaskapp.py
+++ newsroom/flaskapp.py
@@ -9,6 +9,7 @@ import os
 import eve
 import importlib
 
+from superdesk.storage import AmazonMediaStorage, SuperdeskGridFSMediaStorage
 from superdesk.datalayer import SuperdeskDataLayer
 
 
@@ -18,7 +19,7 @@ NEWSROOM_DIR = os.path.abspath(os.path.dirname(__file__))
 class Newsroom(eve.Eve):
     """The main Newsroom object."""
 
-    def __init__(self, import_name=__package__, settings='settings.py',
+    def __init__(self, import_name=__package__, settings={},
                  **kwargs):
         super(Newsroom, self).__init__(
             import_name,
@@ -27,11 +28,16 @@ class Newsroom(eve.Eve):
             template_folder=os.path.join(NEWSROOM_DIR, 'templates'),
             static_folder=os.path.join(NEWSROOM_DIR, 'static'),
             **kwargs)
+        if self.config.get('AMAZON_CONTAINER_NAME'):
+            self.media = AmazonMediaStorage(self)
+        else:
+            self.media = SuperdeskGridFSMediaStorage(self)
         self._setup_blueprints(self.config['MODULES'])
 
     def load_config(self):
         super(Newsroom, self).load_config()
         self.config.from_object('newsroom.default_settings')
+        self.config.from_envvar('NEWSROOM_SETTINGS', silent=True)
 
     def _setup_blueprints(self, modules):
         for name in modules:
