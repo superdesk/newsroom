16a7af449bfbaff77e4b021320b9a850734347d4
Petr Jasek <jasekpetr@gmail.com>
2017-08-17 15:27:23 +0200

    list wire items on homepage

--- newsroom/__init__.py
+++ newsroom/__init__.py
@@ -5,4 +5,8 @@ Superdesk Newsroom
 :license: GPLv3
 """
 
+# reuse content api dbs
+MONGO_PREFIX = 'CONTENTAPI_MONGO'
+ELASTIC_PREFIX = 'CONTENTAPI_ELASTICSEARCH'
+
 from newsroom.flaskapp import Newsroom  # noqa
---
+++ newsroom/utils.py
@@ -0,0 +1,14 @@
+
+from flask import current_app as app
+from eve.utils import parse_request
+
+
+def query_resource(resource, lookup=None, max_results=0):
+    req = parse_request(resource)
+    req.max_results = max_results
+    return app.data.find(resource, req, lookup)
+
+
+def find_one(resource, **lookup):
+    req = parse_request(resource)
+    return app.data.find_one(resource, req, **lookup)
