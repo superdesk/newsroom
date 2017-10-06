af8956354b4e417e8693d023f0077565bc74a172
Aleksandar Jelicic <aleksandar.jelicic@sourcefabric.org>
2017-10-06 17:08:50 +0200

    layout, design wire list item, css changes (#37)
    * layout, design wire list item, css changes

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,44 @@
 // @flow
+
+// Toggle left bar navigation
+
+document.getElementsByClassName('content-bar__menu--nav')[0].onclick = function(){
+    document.getElementsByClassName('wire-column__nav')[0].classList.toggle('wire-column__nav--open');
+    document.getElementsByClassName('content-bar__menu--nav')[0].classList.toggle('content-bar__menu--nav--open');
+};
+
+
+// Open article from wire list
+
+var listItem = document.getElementsByClassName('wire-articles__item');
+
+var currentItem;
+for(var i = 0; i < listItem.length; i++) {
+    listItem[i].onclick = function(event) {
+        if (event.target.classList[0] === 'no-bindable') {
+            document.getElementsByClassName('wire-articles__versions')[0].classList.toggle('wire-articles__versions--open');
+            return false;
+        }
+
+        if (currentItem !== this) {
+            document.getElementsByClassName('wire-column__preview')[0].classList.add('wire-column__preview--open');
+            return currentItem = this;
+        } else {
+            document.getElementsByClassName('wire-column__preview')[0].classList.remove('wire-column__preview--open');
+            return currentItem = null;
+        }
+    };
+}
+
+// Top bar search items
+
+var searchForm = document.getElementsByClassName('search__form')[0];
+var searchInput = document.getElementsByClassName('search__input')[0];
+
+searchInput.onfocus = function() {
+    searchForm.classList.add('searchForm--active');
+};
+
+document.getElementsByClassName('search__clear')[0].onclick = function() {
+    searchInput.value = '';
+};
