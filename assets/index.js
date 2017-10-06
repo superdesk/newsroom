199e633bd0c5d006816dce66515120f59ad47856
aleksandarjelicic <aleksandar.jelicic@sourcefabric.org>
2017-10-06 16:30:59 +0200

    test fix

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,7 @@
 document.getElementsByClassName('content-bar__menu--nav')[0].onclick = function(){
     document.getElementsByClassName('wire-column__nav')[0].classList.toggle('wire-column__nav--open');
     document.getElementsByClassName('content-bar__menu--nav')[0].classList.toggle('content-bar__menu--nav--open');
-}
+};
 
 
 // Open article from wire list
@@ -27,7 +27,7 @@ for(var i = 0; i < listItem.length; i++) {
             document.getElementsByClassName('wire-column__preview')[0].classList.remove('wire-column__preview--open');
             return currentItem = null;
         }
-    }
+    };
 }
 
 // Top bar search items
@@ -37,8 +37,7 @@ var searchInput = document.getElementsByClassName('search__input')[0];
 
 searchInput.onfocus = function() {
     searchForm.classList.add('searchForm--active');
-}
+};
 
 document.getElementsByClassName('search__clear')[0].onclick = function() {
     searchInput.value = '';
-}
