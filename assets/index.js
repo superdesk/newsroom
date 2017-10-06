1b020e40d03f6303fe4661ffaeb0cfe82f660021
aleksandarjelicic <aleksandar.jelicic@sourcefabric.org>
2017-10-06 15:36:02 +0200

    layout changes

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,25 @@
 // @flow
+
+// Toggle left bar navigation
+
 document.getElementsByClassName('content-bar__menu--nav')[0].onclick = function(){
     document.getElementsByClassName('wire-column__nav')[0].classList.toggle('wire-column__nav--open');
     document.getElementsByClassName('content-bar__menu--nav')[0].classList.toggle('content-bar__menu--nav--open');
 }
 
+
+// Open article from wire list
+
 var listItem = document.getElementsByClassName('wire-articles__item');
 
 var currentItem;
 for(var i = 0; i < listItem.length; i++) {
-    listItem[i].onclick = function() {
+    listItem[i].onclick = function(event) {
+        if (event.target.classList[0] === 'no-bindable') {
+            document.getElementsByClassName('wire-articles__versions')[0].classList.toggle('wire-articles__versions--open');
+            return false;
+        }
+
         if (currentItem !== this) {
             document.getElementsByClassName('wire-column__preview')[0].classList.add('wire-column__preview--open');
             return currentItem = this;
@@ -18,6 +29,8 @@ for(var i = 0; i < listItem.length; i++) {
         }
     }
 }
+
+// Top bar search items
 
 var searchForm = document.getElementsByClassName('search__form')[0];
 var searchInput = document.getElementsByClassName('search__input')[0];
