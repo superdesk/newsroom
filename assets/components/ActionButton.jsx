1990876f66c40ae39c0e5e22865db6925217601e
Tolga Akin <akint@aap.com.au>
2017-11-20 22:12:22 +1100

    Adding short cut action buttons

---
+++ assets/components/ActionButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function ActionButton({item, className, displayName, action}) {
+    return (
+        <button
+            key={action.name}
+            type='button'
+            className={className}
+            onClick={() => action.action(action.multi ? [item._id] : item)}
+            title={!displayName && action.name}>
+            <i className={`icon--${action.icon}`}></i>
+            {displayName && action.name}</button>
+    );
+}
+
+ActionButton.propTypes = {
+    item: PropTypes.object,
+    className: PropTypes.string,
+    displayName: PropTypes.bool,
+    action: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+        action: PropTypes.func.isRequired,
+    })
+};
+
+export default ActionButton;
