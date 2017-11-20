02912bbeef63669e7e34e9a903a2d2c225704de6
Petr <petr.jasek@sourcefabric.org>
2017-11-20 10:10:23 +0100

    implement refresh btn in search results tab

--- assets/wire/components/SearchResultsInfo.jsx
+++ assets/wire/components/SearchResultsInfo.jsx
@@ -5,28 +5,46 @@ import { isEmpty } from 'lodash';
 import { gettext } from 'utils';
 import { getActiveQuery, isTopicActive } from 'wire/utils';
 
-
-function SearchResultsInfo({user, query, totalItems, followTopic, topics, bookmarks, activeFilter, createdFilter}) {
+function SearchResultsInfo({
+    user,
+    query,
+    totalItems,
+    followTopic,
+    topics,
+    bookmarks,
+    activeFilter,
+    createdFilter,
+    newItems,
+    refresh
+}) {
     const activeQuery = getActiveQuery(query, activeFilter, createdFilter);
     const isFollowing = user && topics.find((topic) => isTopicActive(topic, activeQuery));
     return (
-        <div className="navbar mt-3 p-0">
+        <div className="d-flex mt-3 p-0 align-items-center">
             <div className="navbar-text search-results-info">
                 <span className="search-results-info__num">{totalItems}</span>
                 {query && (
-                    <span>
+                    <span className="search-results-info__text">
                         {gettext('search results for:')}<br />
                         <b>{query}</b>
                     </span>
                 )}
             </div>
+
             {user && !bookmarks && !isEmpty(activeQuery) && (
                 <button
                     disabled={isFollowing}
-                    className="btn btn-outline-primary"
+                    className="btn btn-outline-primary mr-auto"
                     onClick={() => followTopic(activeQuery)}
                 >{gettext('Save as topic')}</button>
             )}
+
+            {!isEmpty(newItems) &&
+                <button type="button" className="button__reset-styles d-flex align-items-center ml-auto" onClick={refresh}>
+                    <i className="icon--refresh icon--pink" />
+                    <span className="badge badge-pill badge-info badge-secondary ml-2">{newItems.length}</span>
+                </button>
+            }
         </div>
     );
 }
@@ -40,6 +58,8 @@ SearchResultsInfo.propTypes = {
     bookmarks: PropTypes.bool,
     activeFilter: PropTypes.object,
     createdFilter: PropTypes.object,
+    newItems: PropTypes.array,
+    refresh: PropTypes.func,
 };
 
 export default SearchResultsInfo;
