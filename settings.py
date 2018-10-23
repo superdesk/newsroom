04f3ff88e75550a48a472f1f644b9b743daf870e
Petr Jasek <jasekpetr@gmail.com>
2018-10-23 11:21:59 +0200

    initial superdesk newshub config

---
+++ settings.py
@@ -0,0 +1,6 @@
+SITE_NAME = 'Sourcefabric Newshub'
+COPYRIGHT_HOLDER = 'Sourcefabric'
+CONTACT_ADDRESS = 'https://www.superdesk.org/publishing/newshub'
+PRIVACY_POLICY = ''
+TERMS_AND_CONDITIONS = ''
+SHOW_COPYRIGHT = False
