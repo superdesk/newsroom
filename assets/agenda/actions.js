4a0b308f79f448caafdad62aa0cac801638a1f78
Tolga Akin <akint@aap.com.au>
2018-11-08 22:15:22 +1100

    Existing items were not auto updating

--- assets/agenda/actions.js
+++ assets/agenda/actions.js
@@ -337,7 +337,10 @@ export function pushNotification(push) {
             return dispatch(setNewItemsByTopic(push.extra));
 
         case 'new_item':
-            return dispatch(setNewItems(push.extra));
+            return new Promise((resolve, reject) => {
+                dispatch(updateItems(push.extra));
+                dispatch(fetchNewItems()).then(resolve).catch(reject);
+            });
 
         case `topics:${user}`:
             return dispatch(reloadTopics(user));
@@ -367,6 +370,11 @@ function setTopics(topics) {
 export const SET_NEW_ITEMS = 'SET_NEW_ITEMS';
 export function setNewItems(data) {
     return {type: SET_NEW_ITEMS, data};
+}
+
+export const UPDATE_ITEMS = 'UPDATE_ITEMS';
+export function updateItems(data) {
+    return {type: UPDATE_ITEMS, data};
 }
 
 export function fetchNewItems() {
