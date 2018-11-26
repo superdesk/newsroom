db8e01822fc238264d49e46c6d84250d06252f77
Petr Jasek <jasekpetr@gmail.com>
2018-11-26 11:19:15 +0100

    update links in footer for stt
    STTNHUB-44 STTNHUB-45

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
 SITE_NAME = 'Mediapankki'
 COPYRIGHT_HOLDER = 'STT'
+
+PRIVACY_POLICY = PRIVACY_POLICY_EN = 'https://stt.fi/tietosuoja/'
+TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_EN = 'https://stt.fi/kayttoehdot/'
+CONTACT_ADDRESS = 'https://stt.fi/yhteystiedot/'
+CONTACT_ADDRESS_EN = 'https://stt.fi/en/contact/'
