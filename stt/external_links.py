30a6a97421ad608918cda9647b3192fc75ac8f30
Petr <petr.jasek@sourcefabric.org>
2018-12-03 12:10:42 +0100

    setup stt external links
    STTNHUB-39

---
+++ stt/external_links.py
@@ -0,0 +1,5 @@
+
+def init_app(app):
+    app.sidenav('STT Pro', url='https://pro.stt.fi', icon='calendar', group=8)
+    app.sidenav('Lehtikuvan Kuvakauppa', url='http://kuvakauppa.lehtikuva.fi', icon='photo', group=8)
+    app.sidenav('STT Info -tiedotejakelu', url='https://www.sttinfo.fi', icon='info', group=8)
