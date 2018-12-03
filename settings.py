7547b139da92aab5574a41b79f00b05ce18eb864
Petr <petr.jasek@sourcefabric.org>
2018-12-03 12:10:42 +0100

    setup stt external links
    STTNHUB-39

--- settings.py
+++ settings.py
@@ -5,3 +5,7 @@ PRIVACY_POLICY = PRIVACY_POLICY_EN = 'https://stt.fi/tietosuoja/'
 TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_EN = 'https://stt.fi/kayttoehdot/'
 CONTACT_ADDRESS = 'https://stt.fi/yhteystiedot/'
 CONTACT_ADDRESS_EN = 'https://stt.fi/en/contact/'
+
+INSTALLED_APPS = [
+    'stt.external_links',
+]
