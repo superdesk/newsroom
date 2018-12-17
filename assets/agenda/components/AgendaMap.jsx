09fe7368ec495086e5dc684ea60a66620dc514e8
darconny <darko.tomic@gmail.com>
2018-12-17 15:20:38 +0100

    Agenda detail styling

--- assets/agenda/components/AgendaMap.jsx
+++ assets/agenda/components/AgendaMap.jsx
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types';
 export default function AgendaMap({image}) {
     return (
         image && <figure className="wire-column__preview__image">
-            {image}
+            <span>
+                {image}
+            </span>            
         </figure>
     );
 }
