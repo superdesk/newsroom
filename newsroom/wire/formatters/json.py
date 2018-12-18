afc6c19835983c984d0ebdfdb9bc54d18f0f2e8e
Tolga Akin <akint@aap.com.au>
2018-12-18 14:50:36 +1100

    Used a white list

--- newsroom/wire/formatters/json.py
+++ newsroom/wire/formatters/json.py
@@ -4,34 +4,18 @@ from copy import deepcopy
 from flask import json
 from newsroom.formatter import BaseFormatter
 
-agenda_remove_fields = {
-    'bookmarks',
-    'copies',
-    'shares',
-    'event',
-    'event_id',
-    'planning_items',
-    'display_dates',
-    '_id',
-    '_updated',
-    '_created',
-    'priority',
-    'event_contact_info',
-    'state',
-    'version',
-    'recurrence_id',
-    'guid',
-    'ednote',
-    'location',
-    'definition_short',
-    'versioncreated',
-    'products',
-    'firstcreated',
-    'urgency',
-    'calendars',
-    'abstract',
+agenda_json_fields = [
+    'name',
+    'slugline',
+    'headline',
+    'definition',
+    'dates',
+    'coverages',
     'service',
-}
+    'category',
+    'place',
+    'content_type',
+]
 
 
 class JsonFormatter(BaseFormatter):
@@ -55,8 +39,6 @@ class JsonFormatter(BaseFormatter):
 
         output_item = deepcopy(item)
         output_item['event_contact_info'] = self.formatter._expand_contact_info(item)
-        for f in self.formatter.remove_fields.union(agenda_remove_fields):
-            output_item.pop(f, None)
 
         self.format_coverages(output_item)
 
@@ -68,14 +50,13 @@ class JsonFormatter(BaseFormatter):
             output_item['category'] = [{
                 'name': s.get('name')} for s in output_item.get('subject', [])
                 if s.get('name')]
-        output_item.pop('subject', None)
 
         if output_item.get('definition_long'):
             output_item['definition'] = output_item.get('definition_long')
-        output_item.pop('definition_long', None)
 
         if output_item.get('genre'):
             output_item['content_type'] = output_item.get('genre')
-        output_item.pop('genre', None)
 
-        return json.dumps(output_item, indent=2).encode()
+        filtered_output_item = {k: output_item[k] for k in agenda_json_fields if k in output_item}
+
+        return json.dumps(filtered_output_item, indent=2).encode()
