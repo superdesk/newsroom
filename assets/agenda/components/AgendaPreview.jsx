d9e8d498ddfffce679125753112fb3ffebc85730
nrvikas@gmail.com <nrvikas@gmail.com>
2019-01-23 22:25:58 +1100

    [SDAN-423] Planning Item's (without event) coverage was not displayed when 'open' action was chosen from its preview

--- assets/agenda/components/AgendaPreview.jsx
+++ assets/agenda/components/AgendaPreview.jsx
@@ -62,7 +62,7 @@ class AgendaPreview extends React.PureComponent {
                 {item &&
                     <Preview onCloseClick={this.props.closePreview} published={item.versioncreated}>
                         <div className='wire-column__preview__top-bar'>
-                            <PreviewActionButtons item={item} user={user} actions={actions} />
+                            <PreviewActionButtons item={item} user={user} actions={actions} plan={previewPlan} group={previewGroup} />
                         </div>
 
                         <div id='preview-article' className='wire-column__preview__content pt-0' ref={(preview) => this.preview = preview}>
