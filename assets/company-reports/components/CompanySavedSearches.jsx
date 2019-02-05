1048646823529d91b94f7b027266956f1fc16796
nrvikas@gmail.com <nrvikas@gmail.com>
2019-02-05 20:13:06 +1100

    [SDAN-434] Changes to reports UI table
    border change

--- assets/company-reports/components/CompanySavedSearches.jsx
+++ assets/company-reports/components/CompanySavedSearches.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReportsTable from './ReportsTable';
+
 import { gettext } from 'utils';
 
 
-function CompanySavedSearches({data}) {
+function CompanySavedSearches({data, print}) {
     const list = data.results && data.results.map((item) =>
         <tr key={item._id}>
             <td>{item.name}</td>
@@ -12,26 +14,13 @@ function CompanySavedSearches({data}) {
         </tr>
     );
 
-    return (
-        <section className="content-main">
-            <div className="list-items-container">
-                {data.results && <table className="table table-bordered">
-                    <thead className="thead-light">
-                        <tr>
-                            <th>{ gettext('Company') }</th>
-                            <th>{ gettext('Is Enabled') }</th>
-                            <th>{ gettext('Number Of Saved Searches') }</th>
-                        </tr>
-                    </thead>
-                    <tbody>{list}</tbody>
-                </table>}
-            </div>
-        </section>
-    );
+    const headers = [gettext('Company'), gettext('Is Enabled'), gettext('Number Of Saved Searches')];
+    return data.results ? (<ReportsTable headers={headers} rows={list} print={print} />) : null;
 }
 
 CompanySavedSearches.propTypes = {
     data: PropTypes.object.isRequired,
+    print: PropTypes.bool,
 };
 
 export default CompanySavedSearches;
