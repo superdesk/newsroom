4d977dc8efbe5f5dbe7bf8a6f83b9d1f0813d6af
Tolga Akin <akint@aap.com.au>
2019-04-23 23:14:59 +1000

    Displaying private notes if they exists

--- assets/wire/components/PreviewEdnote.jsx
+++ assets/wire/components/PreviewEdnote.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { gettext } from 'utils';
 import InfoBox from './InfoBox';
 
 
+/**
+ * @return {null}
+ */
 function PreviewEdnote({item, isItemDetail}) {
+    const privateNote = get(item, 'extra.sttnote_private');
     return (
-        item.ednote ? <InfoBox label={gettext('Note')} top={!isItemDetail}>
-            {item.ednote}
+        item.ednote || privateNote ? <InfoBox label={gettext('Note')} top={!isItemDetail}>
+            <div className='column__preview__tags__column'>
+                <div>{item.ednote}</div>
+                <div>{privateNote}</div>
+            </div>
         </InfoBox> : null
     );
 }
