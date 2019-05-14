fdd4ddc92985df4214cd5a7969b97719321247f7
marwoodandrew <amarwood@aap.com.au>
2019-05-14 15:07:02 +1000

    [SDAN-480, SDAN-483] Add last active time for user and filter user list by company

--- assets/users/components/UserListItem.jsx
+++ assets/users/components/UserListItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { gettext, shortDate } from 'utils';
+import { gettext, shortDate, fullDate } from 'utils';
 import {getUserLabel} from '../utils';
+import {get} from 'lodash';
 
 
 function UserListItem({user, isActive, onClick, companiesById}) {
     return (
         <tr key={user._id}
-            className={isActive?'table--selected':null}
+            className={`${isActive?'table--selected':''}
+            ${(user.company && companiesById[user.company] && !get(companiesById[user.company],'is_enabled')) ||
+            !user.is_enabled?'table-secondary':null}`}
             onClick={() => onClick(user._id)}>
             <td className="name">{user.first_name} {user.last_name}</td>
             <td>{user.email}</td>
@@ -15,10 +18,11 @@ function UserListItem({user, isActive, onClick, companiesById}) {
             <td>{user.mobile}</td>
             <td>{user.role}</td>
             <td>{getUserLabel(user.user_type)}</td>
-            <td>{(user.company && companiesById ? companiesById[user.company].name : null)}</td>
+            <td>{(user.company && companiesById ? get(companiesById[user.company],'name') : null)}</td>
             <td>{(user.is_approved ? gettext('Approved') : gettext('Needs Approval'))} -
                 {(user.is_enabled ? gettext('Enabled') : gettext('Disabled'))}</td>
             <td>{shortDate(user._created)}</td>
+            <td>{get(user, 'last_active') ? fullDate(get(user, 'last_active')) : ''}</td>
         </tr>
     );
 }
