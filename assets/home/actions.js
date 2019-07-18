46b91f3469d4816090eeab9de5e879ca5da4da12
Mark Pittaway <mark.pittaway@gmail.com>
2019-07-18 11:50:01 +1000

    [SDAN-532] Fetch card external item details after loading the page

--- assets/home/actions.js
+++ assets/home/actions.js
@@ -1,4 +1,7 @@
 import analytics from 'analytics';
+import server from '../server';
+import {errorHandler} from 'utils';
+import {get} from 'lodash';
 
 export const INIT_DATA = 'INIT_DATA';
 export function initData(data) {
@@ -15,4 +18,19 @@ export function openItemDetails(item) {
 export const SET_ACTIVE = 'SET_ACTIVE';
 export function setActive(cardId) {
     return {type: SET_ACTIVE, cardId};
+}
+
+export const SET_CARD_ITEMS = 'SET_CARD_ITEMS';
+export function setCardItems(cardLabel, items) {
+    return {type: SET_CARD_ITEMS, payload: {card: cardLabel, items: items}};
+}
+
+export function fetchCardExternalItems(cardId, cardLabel) {
+    return (dispatch) => {
+        return server.get(`/media_card_external/${cardId}`)
+            .then((data) => dispatch(
+                setCardItems(cardLabel, get(data, '_items', []))
+            ))
+            .catch(errorHandler);
+    };
 }
