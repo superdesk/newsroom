eb78794459b18b716dce67d9fc5dda5f940a902d
marwoodandrew <amarwood@aap.com.au>
2019-10-10 16:12:03 +1100

    Initialise the news API app (#862)

---
+++ newsroom/news_api/app/__init__.py
@@ -0,0 +1,51 @@
+import os
+import flask
+import logging
+from eve import Eve
+from eve.io.mongo.mongo import MongoJSONEncoder
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_app(config=None):
+    app_config = flask.Config('.')
+
+    # set some required fields
+    app_config.update({'DOMAIN': {'upload': {}}, 'SOURCES': {}})
+
+    # pickup the default newsroom settings
+    app_config.from_object('newsroom.default_settings')
+
+    try:
+        # override from settings module, but only things defined in default config
+        import newsroom.news_api.settings as server_settings
+        for key in dir(server_settings):
+            if key.isupper() and key in app_config:
+                app_config[key] = getattr(server_settings, key)
+    except ImportError:
+        pass  # if exists
+
+    if config:
+        app_config.update(config)
+
+    # check that the API is enabled to run
+    if not app_config.get('NEWS_API_ENABLED', False):
+        logger.error('News API is not enabled')
+        return None
+
+    app = Eve(json_encoder=MongoJSONEncoder,
+              settings=app_config)
+    return app
+
+
+app = get_app()
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = int(os.environ.get('APIPORT', '5400'))
+    app = get_app()
+    if app.config.get('NEWS_API_ENABLED', False):
+        app.run(host=host, port=port, debug=True, use_reloader=True)
+    else:
+        logger.error('News API is not enabled')
