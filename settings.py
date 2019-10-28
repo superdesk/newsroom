34fd9617d92c9d2f7fd9e2f8da2cd5e311fbdfe6
Tolga Akin <akint@aap.com.au>
2019-10-28 21:25:33 +1100

    Fix for the footer links

--- settings.py
+++ settings.py
@@ -3,9 +3,15 @@ CLIENT_TIME_FORMAT = 'HH:mm'
 CLIENT_DATE_FORMAT = 'MMM DD, YYYY'
 SITE_NAME = 'CP Newsroom LJI'
 SHOW_USER_REGISTER = True
+SHOW_COPYRIGHT = False
 COPYRIGHT_HOLDER = 'CP'
 COPYRIGHT_NOTICE = ''
 USAGE_TERMS = ''
 LANGUAGES = ['en']
 DEFAULT_LANGUAGE = 'en'
 
+PRIVACY_POLICY = PRIVACY_POLICY_EN = 'https://www.thecanadianpress.com/privacy-policy/'
+TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_EN = \
+    'https://www.thecanadianpress.com/content-services/custom-content-creation/terms-conditions/'
+CONTACT_ADDRESS = 'https://www.thecanadianpress.com/contact/'
+CONTACT_ADDRESS_EN = 'https://www.thecanadianpress.com/contact/'
