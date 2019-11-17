6a86a675bd1b7d39f7c5f6daefba5a2ff2ac72dc
Tolga Akin <akint@aap.com.au>
2019-11-17 19:17:41 +1100

    [SDCP-42] - Download button is disabled in home page

--- assets/wire/components/DownloadItemsModal.jsx
+++ assets/wire/components/DownloadItemsModal.jsx
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { gettext } from 'utils';
 import { submitDownloadItems } from 'wire/actions';
 import { modalFormValid } from 'actions';
-import {getFormats, context} from '../../selectors';
+import { context } from '../../selectors';
+import { get } from 'lodash';
 
 import Modal from 'components/Modal';
 import SelectInput from 'components/SelectInput';
@@ -67,8 +68,18 @@ DownloadItemsModal.propTypes = {
     context: PropTypes.string,
 };
 
-const mapStateToProps = (state) => ({
-    options: getFormats(state),
+const mapOptions = (state, props) => {
+    let options = state.formats;
+    if (props.data.items && props.data.items.length) {
+        const itemType = state.format === 'agenda' ? 'agenda' : 'wire';
+        options = options.filter((opt) => get(opt, 'types', ['wire', 'agenda']).includes(itemType));
+    }
+
+    return options.map((format) => ({value: format.format, text: format.name}));
+};
+
+const mapStateToProps = (state, props) => ({
+    options: mapOptions(state, props),
     context: context(state),
 });
 
