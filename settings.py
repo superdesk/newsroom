11b09094fe73ed9ad68e5aa32e5a5eedab7cde06
Tolga Akin <akint@aap.com.au>
2019-11-18 21:08:16 +1100

    [SDCP-107] - Remove unnecessary search parameters from Newshub

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+from flask_babel import gettext
+
 
 CLIENT_TIME_FORMAT = 'HH:mm'
 CLIENT_DATE_FORMAT = 'MMM DD, YYYY'
@@ -15,3 +17,19 @@ TERMS_AND_CONDITIONS = TERMS_AND_CONDITIONS_EN = \
     'https://www.thecanadianpress.com/content-services/custom-content-creation/terms-conditions/'
 CONTACT_ADDRESS = 'https://www.thecanadianpress.com/contact/'
 CONTACT_ADDRESS_EN = 'https://www.thecanadianpress.com/contact/'
+
+WIRE_AGGS = {
+    'service': {'terms': {'field': 'service.name', 'size': 50}},
+    'subject': {'terms': {'field': 'subject.name', 'size': 20}},
+}
+
+WIRE_GROUPS = [
+    {
+        'field': 'service',
+        'label': gettext('Category'),
+    },
+    {
+        'field': 'subject',
+        'label': gettext('Subject'),
+    }
+]
