be32ac619a3ab0e5aff277aa79008e81f5a2bb98
darconny <darko.tomic@gmail.com>
2019-11-22 10:50:41 +0100

    Footer changes

--- newsroom/public/views.py
+++ newsroom/public/views.py
@@ -5,3 +5,6 @@ from newsroom.public import blueprint
 @blueprint.route('/copyright')
 def copyright():
     return flask.render_template('copyright.html')
+@blueprint.route('/terms')
+def terms():
+    return flask.render_template('terms.html')
