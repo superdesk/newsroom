b04574420ef9bcc562e4440da123907edfcdb4e2
vikas <nrvikas@gmail.com>
2019-12-06 13:51:15 +1100

    [SDAN-620]  Improvements to coverage inquiry email (#913)
    test case fix
    
    footer changes

--- newsroom/agenda/email.py
+++ newsroom/agenda/email.py
@@ -5,6 +5,7 @@ from newsroom.email import send_email
 from newsroom.utils import get_agenda_dates, get_location_string, get_links, get_public_contacts, url_for_agenda
 from newsroom.template_filters import is_admin_or_internal
 from newsroom.settings import get_settings_collection, GENERAL_SETTINGS_LOOKUP
+from newsroom.companies import get_user_company
 
 
 def send_coverage_notification_email(user, agenda, wire_item):
@@ -63,14 +64,18 @@ def send_coverage_request_email(user, message, item):
     recipients = general_settings.get('values').get('coverage_request_recipients').split(',')
     assert recipients
     assert isinstance(recipients, list)
-    url = url_for_agenda({'_id': item}, _external=True)
+    url = url_for_agenda({'_id': item['_id']}, _external=True)
     name = '{} {}'.format(user.get('first_name'), user.get('last_name'))
     email = user.get('email')
 
-    subject = gettext('A new coverage request')
-    text_body = render_template('coverage_request_email.txt', name=name, email=email,
-                                message=message, url=url)
-    html_body = render_template('coverage_request_email.html', name=name, email=email,
-                                message=message, url=url)
+    subject = gettext('Coverage inquiry: {}'.format(item.get('name') or item.get('slugline')))
+    user_company = get_user_company(user)
+    if user_company:
+        user_company = user_company.get('name')
+
+    text_body = render_template('coverage_request_email.txt', name=name, email=email, message=message, url=url,
+                                company=user_company, recipients=recipients, subject=subject)
+    html_body = render_template('coverage_request_email.html', name=name, email=email, message=message, url=url,
+                                company=user_company, recipients=recipients, subject=subject)
 
     send_email(to=recipients, subject=subject, text_body=text_body, html_body=html_body)
