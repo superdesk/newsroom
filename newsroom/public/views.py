330935583f9b93ec5f7f70d72025d4e82ebbe6b1
darconny <darko.tomic@gmail.com>
2019-12-25 14:37:44 +0100

    SDCP-124

--- newsroom/public/views.py
+++ newsroom/public/views.py
@@ -2,6 +2,16 @@ import flask
 from newsroom.public import blueprint
 
 
-@blueprint.route('/copyright')
-def copyright():
-    return flask.render_template('copyright.html')
+@blueprint.route('/privacy')
+def privacy():
+    return flask.render_template('privacy.html')
+
+
+@blueprint.route('/terms')
+def terms():
+    return flask.render_template('terms.html')
+
+
+@blueprint.route('/contact')
+def contact():
+    return flask.render_template('contact.html')
