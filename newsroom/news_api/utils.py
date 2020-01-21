22d1ef016802f9c8f5b57eb3453cdd7ec2a0e6f4
nrvikas@gmail.com <nrvikas@gmail.com>
2020-01-21 14:03:16 +1100

    fixed test case

--- newsroom/news_api/utils.py
+++ newsroom/news_api/utils.py
@@ -9,7 +9,7 @@ def post_api_audit(doc):
         'items_id': [doc.get('_id')] if doc.get('_id') else [i.get('_id') for i in doc.get('_items', [])],
         'remote_addr': request.access_route[0] if request.access_route else request.remote_addr,
         'uri': request.url,
-        'endpoint': request.endpoint.replace('|resource', '')
+        'endpoint': (request.endpoint or '').replace('|resource', '')
     }
 
     # g.user contains comapny._id from CompanyTokenAuth.check_auth]
