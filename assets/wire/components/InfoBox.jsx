3a20b1f74ed7d58d79f50cf75000f69da5e444bd
nrvikas@gmail.com <nrvikas@gmail.com>
2020-02-17 10:22:54 +1100

    [SDAN-655] Previewing an agenda item with a coverage crashes react

--- assets/wire/components/InfoBox.jsx
+++ assets/wire/components/InfoBox.jsx
@@ -8,7 +8,7 @@ export default function InfoBox(props) {
     const className = bem('info-box', null, {
         'top': props.top,
     });
-    const renderChildren = (props.children || []).filter((c) => c);
+    const renderChildren = (Array.isArray(props.children) ? props.children : [props.children] || []).filter((c) => c);
 
     return (
         <div className={className} id={props.id || null}>
