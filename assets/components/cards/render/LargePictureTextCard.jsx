29b0fadb7bf1d289fe731351a0eeceea8610bf63
Darko Tomic <darko.tomic@gmail.com>
2020-03-25 11:40:05 +0100

    Fixing aspect ratio in 3-picture-text card (#1021)

--- assets/components/cards/render/LargePictureTextCard.jsx
+++ assets/components/cards/render/LargePictureTextCard.jsx
@@ -13,9 +13,11 @@ const getPictureTextPanel = (item, picture, openItem, cardId) => {
 
     return (<div key={item._id} className="col-sm-6 col-lg-4 d-flex mb-4">
         <div className="card card--home" onClick={() => openItem(item, cardId)}>
-            <div className="card-img-top-wrapper card-img-top-wrapper--aspect-16-9">
-                <img className="card-img-top" src={imageUrl} alt={caption} />
-            </div>
+            {rendition &&
+                <div className="card-img-top-wrapper card-img-top-wrapper--aspect-16-9">
+                    <img className="card-img-top" src={imageUrl} alt={caption} />
+                </div>
+            }
             <CardBody item={item} displaySource={false} />
             <CardFooter
                 wordCount={wordCount(item)}
