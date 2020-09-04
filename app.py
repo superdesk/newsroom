b8e353ebda83a0619369853f4c4b927a2539ccbe
olegpshenichniy <olegpshenichniy@gmail.com>
2020-09-04 16:36:18 +0200

    Temporary display all resources

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ app = NewsroomWebApp(__name__)
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = int(os.environ.get('PORT', '5050'))
+
+    print('\n'.join(app.config['DOMAIN'].keys()))
+
     app.run(debug=True, host=host, port=port, threaded=True)
