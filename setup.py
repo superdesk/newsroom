0c84fad2014f74e0397cd6bf8643c635224405cc
olegpshenichniy <olegpshenichniy@gmail.com>
2020-09-04 16:14:12 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'Babel>=2.5.3,<3.0',
-    'eve==0.7.8',
-    'eve-elastic==2.5.0',
     'WTForms==2.2.1',
-    'flask>=0.12,<1.0',
-    'flask-babel>=0.11.2,<0.12',
     'flask-webpack>=0.1.0,<0.2',
     'Flask-WTF>=0.14.2,<0.15',
     'flask-limiter>=0.9.5.1,<0.9.6',
@@ -14,11 +10,9 @@ install_requires = [
     'flask_pymongo>=0.5.2,<1.0',
     'honcho>=1.0.1',
     'gunicorn>=19.7.1',
-    'superdesk-core>=1.31.1,<=1.32',
     'icalendar>=4.0.3,<4.1',
     'PyRTF3>=0.47.5',
     'xhtml2pdf>=0.2.4',
-    'werkzeug>=0.9.4,<=0.11.15',
 ]
 
 setup(
@@ -32,7 +26,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requires,
-    dependency_links=['http://github.com/superdesk/superdesk-planning/tarball/master#egg=superdesk-planning'],
     scripts=['manage.py'],
     classifiers=[
         'Development Status :: 4 - Beta',
