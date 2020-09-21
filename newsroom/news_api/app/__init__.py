e6037bbc75b94760c352c7935d546ac7d8808b8a
olegpshenichniy <olegpshenichniy@gmail.com>
2020-09-21 16:25:34 +0200

    Fix error handling

--- newsroom/news_api/app/__init__.py
+++ newsroom/news_api/app/__init__.py
@@ -79,7 +79,7 @@ class NewsroomNewsAPI(NewsroomApp):
             })
 
         def base_exception_error(err):
-            if err.error == 'search_phase_execution_exception':
+            if err.args and err.args[0] == 'search_phase_execution_exception':
                 return json_error({
                     'error': 1,
                     'message': 'Invalid search query',
