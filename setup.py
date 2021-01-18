90eb8af0491bf407e0111f83fe7c6ebed9ddfbb9
pablopunk <pablo@pablopunk.com>
2021-01-18 13:08:47 +0100

    Fix dependencies on master

--- setup.py
+++ setup.py
@@ -2,23 +2,20 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'Babel>=2.5.3,<3.0',
-    'eve==0.7.8',
-    'eve-elastic==2.5.0',
-    'WTForms==2.2.1',
-    'flask>=0.12,<1.0',
-    'flask-babel>=0.11.2,<0.12',
     'flask-webpack>=0.1.0,<0.2',
     'Flask-WTF>=0.14.2,<0.15',
     'flask-limiter>=0.9.5.1,<0.9.6',
     'Flask-Cache>=0.13.1,<0.14',
-    'flask_pymongo>=0.5.2,<1.0',
     'honcho>=1.0.1',
     'gunicorn>=19.7.1',
-    'superdesk-core>=1.31.1,<=1.32',
-    'icalendar>=4.0.3,<4.1',
     'PyRTF3>=0.47.5',
     'xhtml2pdf>=0.2.4',
     'werkzeug>=0.9.4,<=0.11.15',
+    'reportlab==3.5.53',
+    'email-validator==1.0.5',
+    'urllib3<1.26',
+    'superdesk-planning==1.33.2',
+    'superdesk-core>=1.33,<1.34',
 ]
 
 setup(
