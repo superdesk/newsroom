24fdea7b05e9aeb8d782987a74c73966907586a1
Mark Pittaway <mark.pittaway@gmail.com>
2021-03-18 15:27:08 +1100

    test

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from newsroom.web import NewsroomWebApp
 app = NewsroomWebApp(__name__)
 
 if __name__ == '__main__':
-    host = '0.0.0.0'
+    host = '0.0.0.0' # test
     port = int(os.environ.get('PORT', '5050'))
     app.run(debug=True, host=host, port=port, threaded=True)
