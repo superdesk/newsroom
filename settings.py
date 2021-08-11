d3d49c6176aad1324dc4fdc532c37423aa8c6168
Mark Pittaway <mark.pittaway@gmail.com>
2021-08-11 17:34:52 +1000

    [CUPR-18] Set KUNA logo and site name

---
+++ settings.py
@@ -0,0 +1,14 @@
+from newsroom.default_settings import ELASTICSEARCH_SETTINGS
+from newsroom.default_settings import CELERY_BEAT_SCHEDULE as CELERY_BEAT_SCHEDULE_DEFAULT
+
+SITE_NAME = 'KUNA Newsroom'
+COPYRIGHT_HOLDER = 'KUNA'
+
+PRIVACY_POLICY = 'https://www.kuna.net.kw/CMSPage.aspx?CategoryID=83'
+TERMS_AND_CONDITIONS = 'https://www.kuna.net.kw/CMSPage.aspx?CategoryID=83'
+CONTACT_ADDRESS = 'https://www.kuna.net.kw/CMSPage.aspx?CategoryID=17'
+
+LANGUAGES = ['ar', 'en']
+DEFAULT_LANGUAGE = 'ar'
+
+WATERMARK_IMAGE = None
