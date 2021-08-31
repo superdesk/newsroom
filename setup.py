1ac56ca0aab21c5de57889efa6d7ebfe8bc53c4c
Petr Jasek <jasekpetr@gmail.com>
2021-08-31 14:11:27 +0200

    fix build

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'Babel>=2.5.3,<3.0',
+    'Babel==2.9.1',
     'eve==0.7.8',
     'eve-elastic==2.5.0',
     'WTForms==2.2.1',
     'flask>=0.12,<1.0',
-    'flask-babel>=0.11.2,<0.12',
+    'flask-babel==0.12.2',
     'flask-webpack>=0.1.0,<0.2',
     'Flask-WTF>=0.14.2,<0.15',
     'flask-limiter>=0.9.5.1,<0.9.6',
     'Flask-Cache>=0.13.1,<0.14',
     'flask_pymongo>=0.5.2,<1.0',
-    'honcho>=1.0.1',
-    'gunicorn>=19.7.1',
-    'superdesk-core>=1.31.1,<=1.32',
-    'icalendar>=4.0.3,<4.1',
-    'PyRTF3>=0.47.5',
-    'xhtml2pdf>=0.2.4',
-    'werkzeug>=0.9.4,<=0.11.15',
+    'honcho==1.0.1',
+    'gunicorn==20.1.0',
+    'superdesk-core==1.33.14',
+    'icalendar==4.0.7',
+    'PyRTF3==0.47.5',
+    'xhtml2pdf==0.2.5',
+    'werkzeug==0.11.15',
 ]
 
 setup(
@@ -32,7 +32,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requires,
-    dependency_links=['http://github.com/superdesk/superdesk-planning/tarball/master#egg=superdesk-planning'],
     scripts=['manage.py'],
     classifiers=[
         'Development Status :: 4 - Beta',
